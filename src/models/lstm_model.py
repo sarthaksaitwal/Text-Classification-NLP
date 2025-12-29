cc0222b106e965e25a85a633567bb8885a1a9672
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2025-12-29 13:58:20 +0530

    added lstm model

---
+++ src/models/lstm_model.py
@@ -0,0 +1,57 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import(
+    Embedding,
+    LSTM,
+    Dense,
+    Dropout
+)
+
+def build_lstm_model(
+    vocab_size:int,
+    max_length:int,
+    embedding_dim:int=128,
+    lstm_units:int=128,
+    num_classes:int=4
+):
+    """
+    Builds and returns an LSTM-based text classification model
+    """
+
+    model=Sequential()
+
+    # Embedding layer
+    model.add(
+        Embedding(
+            input_dim=vocab_size,
+            output_dim=embedding_dim,
+            input_length=max_length
+        )
+    )
+
+    # LSTM layer
+    model.add(
+        LSTM(
+            lstm_units,
+            return_sequences=False
+        )
+    )
+
+    # Dropout for regularization
+    model.add(Dropout(0.5))
+
+    # Output layer
+    model.add(
+        Dense(
+            num_classes,
+            activation="softmax"
+        )
+    )
+
+    # Compile model
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
